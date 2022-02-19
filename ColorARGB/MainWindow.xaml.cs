f4d7d9b801807724cb66283fd25e0312bda62a7e
Pili_zr36 <Pili_zr36@itstepru.onmicrosoft.com>
2022-02-19 22:43:13 +0700

    Add project files.

---
+++ ColorARGB/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ColorARGB
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var colors = new List<string> { "red", "black", "yellow" };
+            Select_Color.ItemsSource = colors;
+        }
+        private void Show_Info_OnClick(object sender, RoutedEventArgs e)
+        {
+            var info = $"Width = {Rectangle.Width}\n";
+            info += $"Height = {Rectangle.Height}\n";
+            info += $"Color = {Rectangle.Fill}";
+            MessageBox.Show(info);
+        }
+
+        private void Clone_OnClick(object sender, RoutedEventArgs e)
+        {
+            var clone = new Rectangle
+            {
+                Fill = Rectangle.Fill,
+                Width = Rectangle.Width,
+                Height = Rectangle.Height,
+                Margin = Rectangle.Margin
+            };
+            Panel.Children.Add(clone);
+        }
+    }
+}
